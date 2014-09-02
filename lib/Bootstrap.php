51aa67cd1e56e27bb5e63c78a0a89b5f5bc920c5
--add <maxim.peshkov@usabilitydynamics.com>
2014-09-02 17:10:27 +0300

    implemented dependencies checker for composer modules

--- lib/Bootstrap.php
+++ lib/Bootstrap.php
@@ -21,6 +21,15 @@ namespace UsabilityDynamics\WP {
     class Bootstrap {
 
       /**
+       * Plugin ( Theme ) Name.
+       *
+       * @public
+       * @property $name
+       * @type string
+       */
+      public $name = false;
+    
+      /**
        * Core version.
        *
        * @static
@@ -37,7 +46,26 @@ namespace UsabilityDynamics\WP {
        * @var string
        */
       public $domain = false;
+      
+      /**
+       * Schemas
+       *
+       * @public
+       * @property schema
+       * @var array
+       */
+      public $schemas = array();
 
+      /**
+       * Errors
+       *
+       * @public
+       * @static
+       * @property $errors
+       * @type array
+       */
+      public $errors = array();
+      
       /**
        * Singleton Instance Reference.
        *
@@ -46,7 +74,7 @@ namespace UsabilityDynamics\WP {
        * @property $instance
        * @type \UsabilityDynamics\WPP\Bootstrap object
        */
-      private static $instance = null;
+      protected static $instance = null;
 
       /**
        * Settings
@@ -57,36 +85,59 @@ namespace UsabilityDynamics\WP {
        * @type \UsabilityDynamics\Settings object
        */
       private $settings = null;
-
+      
       /**
        * Constructor
+       *
+       * @author peshkov@UD
        */
       private function __construct( $args ) {
+        //** Define schemas here since we can set correct paths directly in property */
+        $this->define_schemas();
+        //** Determine if Composer autoloader is included and modules classes are up to date */
+        $this->check_autoload_dependencies();
+        //** Application initialization. */
         $this->init( $args );
+        //** The last step. Print errors if they exist */
+        if( !empty( $this->errors ) ) {
+          add_action( 'admin_notices', array( $this, 'admin_notices' ) );
+        }
       }
       
       /**
        * Initialize application.
        * Redeclare the method in child class!
        *
+       * @author peshkov@UD
        */
       public function init() {}
 
       /**
+       * Defines property schemas ( $this->schemas )
+       * The list of pathes to schemas files must be set if needed.
+       * Redeclare the method in child class!
+       * 
+       */
+      public function define_schemas() {}
+      
+      /**
        * Determine if instance already exists and Return Instance
        *
+       * @author peshkov@UD
        */
       public static function get_instance( $args = array() ) {
-        if( null === self::$instance ) {
-          self::$instance = new self( $args );
+        $class = get_called_class();
+        if( null === $class::$instance ) {
+          $class::$instance = new $class( $args );
         }
-        return self::$instance;
+        return $class::$instance;
       }
       
       /**
        * @param string $key
        * @param mixed $value
        *
+       * @author peshkov@UD
        * @return \UsabilityDynamics\Settings
        */
       public function set( $key = null, $value = null ) {
@@ -100,6 +151,7 @@ namespace UsabilityDynamics\WP {
        * @param string $key
        * @param mixed $default
        *
+       * @author peshkov@UD
        * @return \UsabilityDynamics\type
        */
       public function get( $key = null, $default = null ) {
@@ -107,6 +159,62 @@ namespace UsabilityDynamics\WP {
           return $value;
         }
         return $this->settings->get( $key, $default );
+      }
+      
+      /**
+       * Returns specific schema from file.
+       *
+       * @param string $file Path to file
+       * @param array $l10n Locale data
+       * @author peshkov@UD
+       * @return array
+       */
+      public static function get_schema( $file = '', $l10n = array() ) {
+        if( !empty( $file ) && file_exists( $file ) ) {
+          return (array)\UsabilityDynamics\Utility::l10n_localize( json_decode( file_get_contents( $file ), true ), (array)$l10n );
+        }
+        return array();
+      }
+      
+      /**
+       * Renders admin notes in case there are errors on bootstrap init
+       *
+       * @author peshkov@UD
+       */
+      public function admin_notices() {
+        if( !empty( $this->errors ) && is_array( $this->errors ) ) {
+          $errors = '<ul style="list-style:disc inside;"><li>' . implode( '</li><li>', $this->errors ) . '</li></ul>';
+          $message = sprintf( __( '<p><b>%s</b> is not active due to following errors:</p> %s' ), $this->name, $errors );
+          echo '<div class="error fade" style="padding:11px;">' . $message . '</div>';
+        }
+      }
+      
+      /**
+       * Maybe determines if Composer autoloader is included and modules classes are up to date
+       *
+       * @author peshkov@UD
+       * @return boolean
+       */
+      private function check_autoload_dependencies() {
+        //** Determine if schema is set */
+        if( empty( $this->schemas[ 'dependencies' ] ) ) {
+          return null;
+        }
+        $dependencies = $this->get_schema( $this->schemas[ 'dependencies' ] );
+        if( !empty( $dependencies ) && is_array( $dependencies ) ) {
+          foreach( $dependencies as $module => $classes ) {
+            if( !empty( $classes ) && is_array( $classes ) ) {
+              foreach( $classes as $class => $v ) {
+                if( !class_exists( $class ) ) {
+                  $this->errors[] = sprintf( __( 'Module <b>%s</b> is not installed or the version is old, class <b>%s</b> does not exist.' ), $module, $class );
+                }
+                if ( '*' != trim( $v ) && ( !property_exists( $class, 'version' ) || $class::$version < $v ) ) {
+                  $this->errors[] = sprintf( __( 'Module <b>%s</b> should be updated to the latest version, class <b>%s</b> must have version <b>%s</b> or higher.' ), $module, $class, $v );
+                }
+              }
+            }
+          }
+        }
       }
       
     }
